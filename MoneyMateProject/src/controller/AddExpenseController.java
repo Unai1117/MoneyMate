54ae636d7472836df7f1b23335383202db03a9b9
Unai López <unailopez1117@gmail.com>
2024-05-05 13:08:31 +0200

    Empieza proceso añadir nueva categoría

--- MoneyMateProject/src/controller/AddExpenseController.java
+++ MoneyMateProject/src/controller/AddExpenseController.java
@@ -10,11 +10,13 @@ import java.net.URL;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
@@ -65,6 +67,10 @@ public class AddExpenseController implements Initializable {
     private ImageView imageView;
     
     private Image scanedImage; 
+    @FXML
+    private CheckBox newCategoryBox;
+    @FXML
+    private TextField newCategoryField;
     /**
      * Initializes the controller class.
      */
@@ -82,6 +88,9 @@ public class AddExpenseController implements Initializable {
                     }
                 };
             });
+            
+            //bind disable property to newCategoryfield
+            newCategoryField.disableProperty().bind(Bindings.not(newCategoryBox.selectedProperty()));
             //get the instance of acount that will give us the user
             acount = Acount.getInstance();
             
@@ -89,9 +98,24 @@ public class AddExpenseController implements Initializable {
             categoryMenu = new ChoiceBox<Category>();
             List<Category> categorias = acount.getUserCategories(); 
             //add categories to the drop down menu to let the user select the desired category
-            for(int i = 0; i < categorias.size(); i++){
-                categoryMenu.getItems().add(categorias.get(i)); 
-            }
+            categoryMenu.getItems().addAll(categorias);
+            //bind disable property
+            /*
+            doneButton.disableProperty().bind(Bindings.createBooleanBinding(
+                    () -> nameField.getText().isEmpty() || 
+                          descriptionField.getText().isEmpty() || 
+                          costField.getText().isEmpty() ||
+                          unitsField.getText().isEmpty() ||
+                          datePicker.getValue() == null ||
+                          categoryMenu.getValue() == null,
+                    nameField.textProperty(),
+                    descriptionField.textProperty(),
+                    costField.textProperty(),
+                    unitsField.textProperty(),
+                    datePicker.valueProperty(),
+                    categoryMenu.valueProperty()
+            ));
+            */
         } catch (Exception e){
             System.out.println(e);
         }
@@ -103,8 +127,13 @@ public class AddExpenseController implements Initializable {
         String name = nameField.getText(); 
         String description = descriptionField.getText(); 
         int units = Integer.parseInt(unitsField.getText());
-        LocalDate date = datePicker.getValue(); 
-        Category category = categoryMenu.getValue();
+        LocalDate date = datePicker.getValue();
+//        if(newCategoryField.getText() != null){
+//            Category category = 
+//        } else {
+        Category category = categoryMenu.getValue(); 
+//        }
+        
         try{
             acount.registerCharge(name, description, cost, units, scanedImage, date, category); 
             Pane mainPane = FXMLLoader.load(getClass().getResource("/view/Main.fxml")); 
