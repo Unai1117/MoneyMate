1228e1db969cd06ea123e38e1ad1ae61b6f237ee
Jorge Pardo <jorgeparpar@gmail.com>
2024-05-26 12:39:13 +0200

    feat: improve register field detection

--- MoneyMateProject/src/controller/RegisterController.java
+++ MoneyMateProject/src/controller/RegisterController.java
@@ -4,43 +4,32 @@
  */
 package controller;
 
-import javafx.stage.*;
-import java.awt.Desktop;
 import javafx.scene.image.Image;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.lang.System.Logger;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.Objects;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
-import javafx.beans.binding.Bindings;
-import javafx.beans.binding.BooleanBinding;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
-import static javafx.scene.input.KeyCode.EQUALS;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
-import javafx.stage.FileChooser;
-import javafx.stage.FileChooser.ExtensionFilter;
 import model.*;
 
 /**
@@ -92,12 +81,12 @@ public class RegisterController implements Initializable {
     private Acount acount;
 
     //properties to control valid fields
-    private BooleanProperty validName;
-    private BooleanProperty validSurname;
-    private BooleanProperty validNickname;
-    private BooleanProperty validEmail;
-    private BooleanProperty validPassword;
-    private BooleanProperty equalPasswords;
+    private BooleanProperty validName = new SimpleBooleanProperty(false);
+    private BooleanProperty validSurname = new SimpleBooleanProperty(false);
+    private BooleanProperty validNickname = new SimpleBooleanProperty(false);
+    private BooleanProperty validEmail = new SimpleBooleanProperty(false);
+    private BooleanProperty validPassword = new SimpleBooleanProperty(false);
+    private BooleanProperty equalPasswords = new SimpleBooleanProperty(false);
 
     private final int EQUALS = 0;
 
@@ -116,33 +105,49 @@ public class RegisterController implements Initializable {
             java.util.logging.Logger.getLogger(RegisterController.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        //Booleans for checking fields
-        validName = new SimpleBooleanProperty();
-        validName.setValue(Boolean.FALSE);
-        
-        validSurname = new SimpleBooleanProperty();
-        validSurname.setValue(Boolean.FALSE);
-        
-        validNickname = new SimpleBooleanProperty();
-        validNickname.setValue(Boolean.FALSE);
-        
-        validEmail = new SimpleBooleanProperty();
-        validEmail.setValue(Boolean.FALSE);
-
-        validPassword = new SimpleBooleanProperty();
-        validPassword.setValue(Boolean.FALSE);
-
-        equalPasswords = new SimpleBooleanProperty();
-        equalPasswords.setValue(Boolean.FALSE);
-
         //BooleanBinding validFields = Bindings.and(validEmail, validPassword).and(equalPasswords);
-
         //register.disableProperty().bind(Bindings.not(validFields));
         stackPane.prefWidthProperty().bind(scrollPane.widthProperty().subtract(20));
-        
+
+        nnickname.focusedProperty().addListener((obs, oldVal, newVal) -> {
+            if (oldVal) {
+                checknickname();
+            }
+        });
+
+        nname.focusedProperty().addListener((obs, oldVal, newVal) -> {
+            if (oldVal) {
+                checkname();
+            }
+        });
+
+        ssurname.focusedProperty().addListener((obs, oldVal, newVal) -> {
+            if (oldVal) {
+                checksurname();
+            }
+        });
+
+        eemail.focusedProperty().addListener((obs, oldVal, newVal) -> {
+            if (oldVal) {
+                checkemail();
+            }
+        });
+
+        password1.focusedProperty().addListener((obs, oldVal, newVal) -> {
+            if (oldVal) {
+                checkpasswords();
+            }
+        });
+
+        password2.focusedProperty().addListener((obs, oldVal, newVal) -> {
+            if (oldVal) {
+                checkpasswords();
+            }
+        });
+
     }
-    
-     //Method that register the user
+
+    //Method that register the user
     @FXML
     private void acceptRegister(MouseEvent event) throws AcountDAOException {
         checkname();
@@ -150,9 +155,8 @@ public class RegisterController implements Initializable {
         checknickname();
         checkemail();
         checkpasswords();
-        if ((Objects.equals(validEmail.getValue(), Boolean.TRUE))) System.out.println("works");
         if ((Objects.equals(validName.getValue(), Boolean.TRUE)) && (Objects.equals(validSurname.getValue(), Boolean.TRUE)) && (Objects.equals(validNickname.getValue(), Boolean.TRUE)) && (Objects.equals(validEmail.getValue(), Boolean.TRUE)) && (Objects.equals(validPassword.getValue(), Boolean.TRUE))) {
-            
+
             String name = nname.textProperty().getValueSafe();
             String surname = ssurname.textProperty().getValueSafe();
             String email = eemail.textProperty().getValueSafe();
@@ -173,7 +177,7 @@ public class RegisterController implements Initializable {
             //handleBAcceptOnAction();
         }
     }
-    
+
     //Go to login page 
     @FXML
     private void goLogin(ActionEvent event) {
@@ -184,7 +188,7 @@ public class RegisterController implements Initializable {
             System.out.println(e);
         }
     }
-    
+
     @FXML
     //obtain image
     private void openFiles(ActionEvent event) {
@@ -195,10 +199,10 @@ public class RegisterController implements Initializable {
 
         }
     }
-    
+
     @FXML
     private void checkname() {
-        if (!nname.textProperty().getValueSafe().isEmpty() ){
+        if (!nname.textProperty().getValueSafe().isEmpty()) {
             nameLabel.getStyleClass().remove("destructive-label");
             nname.getStyleClass().remove("destructive-input");
             nameLabel.setText("Name*");
@@ -209,10 +213,10 @@ public class RegisterController implements Initializable {
             nname.getStyleClass().add("destructive-input");
         }
     }
-    
+
     @FXML
     private void checksurname() {
-        if (!ssurname.textProperty().getValueSafe().isEmpty()){
+        if (!ssurname.textProperty().getValueSafe().isEmpty()) {
             surnameLabel.getStyleClass().remove("destructive-label");
             ssurname.getStyleClass().remove("destructive-input");
             surnameLabel.setText("Surname*");
@@ -223,10 +227,10 @@ public class RegisterController implements Initializable {
             ssurname.getStyleClass().add("destructive-input");
         }
     }
-    
+
     @FXML
     private void checknickname() {
-        if (!nnickname.textProperty().getValueSafe().isEmpty()){
+        if (User.checkNickName(nnickname.textProperty().getValueSafe())) {
             nicknameLabel.getStyleClass().remove("destructive-label");
             nnickname.getStyleClass().remove("destructive-input");
             nicknameLabel.setText("Nickname*");
@@ -236,11 +240,11 @@ public class RegisterController implements Initializable {
             nnickname.getStyleClass().add("destructive-input");
             nicknameLabel.getStyleClass().add("destructive-label");
         }
-    } 
-    
+    }
+
     @FXML
     private void checkemail() {
-        if (User.checkEmail(eemail.textProperty().getValueSafe())){
+        if (User.checkEmail(eemail.textProperty().getValueSafe())) {
             emailLabel.getStyleClass().remove("destructive-label");
             eemail.getStyleClass().remove("destructive-input");
             emailLabel.setText("Email*");
@@ -250,37 +254,35 @@ public class RegisterController implements Initializable {
             emailLabel.getStyleClass().add("destructive-label");
             eemail.getStyleClass().add("destructive-input");
         }
-    } 
-    
+    }
+
     @FXML
     private void checkpasswords() {
-        if (password1.textProperty().getValueSafe().isEmpty()){
+        if (!User.checkPassword(password1.textProperty().getValueSafe())) {
             password1Label.setText("Password* - Invalid Password!");
             password1Label.getStyleClass().add("destructive-label");
             password2Label.getStyleClass().add("destructive-label");
             password1.getStyleClass().add("destructive-input");
             password2.getStyleClass().add("destructive-input");
         } else {
-                password1Label.setText("Password*");
-                password1Label.getStyleClass().remove("destructive-label");
-                password2Label.getStyleClass().remove("destructive-label");
-                password1.getStyleClass().remove("destructive-input");
-                password2.getStyleClass().remove("destructive-input");
-            if (!password1.textProperty().getValueSafe().equals(password2.textProperty().getValueSafe())){
+            password1Label.setText("Password*");
+            password1Label.getStyleClass().remove("destructive-label");
+            password2Label.getStyleClass().remove("destructive-label");
+            password1.getStyleClass().remove("destructive-input");
+            password2.getStyleClass().remove("destructive-input");
+            if (!password1.textProperty().getValueSafe().equals(password2.textProperty().getValueSafe())) {
                 password2Label.setText("Confirm Password* - Passwords don't match!");
                 password2Label.getStyleClass().add("destructive-label");
                 password2.getStyleClass().add("destructive-input");
             } else {
                 password2Label.setText("Confirm Password*");
-                password1Label.setStyle("-fx-text-fill: #eeeeee");
-                password2Label.setStyle("-fx-text-fill: #eeeeee");
                 password1Label.getStyleClass().remove("destructive-label");
                 password2Label.getStyleClass().remove("destructive-label");
                 password1.getStyleClass().remove("destructive-input");
                 password2.getStyleClass().remove("destructive-input");
                 validPassword.setValue(Boolean.TRUE);
-                
+
             }
         }
-    } 
+    }
 }
